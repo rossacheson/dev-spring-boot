786dd2b6bc4919703835b1ba9f59ac77b9804839
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-22 11:42:43 -0600

    Add springcoredemo application from start.spring.io Spring Initializr

---
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.rossacheson.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
