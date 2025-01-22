86da761c55456efcbd4f4183c49c8ab5c9296778
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-22 12:56:59 -0600

    Use a Qualifier to specify a Bean when there are multiple Classes implementing the interface

---
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.rossacheson.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your back hand volley";
+    }
+}
--- 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/rest/DemoController.java
@@ -2,25 +2,26 @@ package com.rossacheson.springcoredemo.rest;
 
 import com.rossacheson.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    private Coach myCoach;
+    private final Coach myCoach;
 
     // Constructor Injection -- the typical recommended approach
     // Note: @Autowired annotation is optional when there is only one constructor
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        this.myCoach = theCoach;
-//    }
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        this.myCoach = theCoach;
+    }
 
     // Setter Injection -- typically only used for optional dependencies
-    @Autowired
-    public void setCoach(Coach coach) {
-        myCoach = coach;
-    }
+//    @Autowired
+//    public void setCoach(Coach coach) {
+//        myCoach = coach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
