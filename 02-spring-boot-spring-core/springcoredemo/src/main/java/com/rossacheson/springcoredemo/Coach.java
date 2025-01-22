0933aa49b1da66b22b6c3c176b4ac1d57948b413
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-22 12:11:37 -0600

    Daily workout endpoint using Coach interface and Spring's Dependency Injection

---
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.rossacheson.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/DemoController.java
@@ -0,0 +1,21 @@
+package com.rossacheson.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private final Coach myCoach;
+
+    // Note: @Autowired annotation is optional when there is only one constructor
+    @Autowired
+    public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return this.myCoach.getDailyWorkout();
+    }
+}
