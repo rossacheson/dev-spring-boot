ef7551112f6c89d6e982c1a0b492ef51dfe1abbe
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-23 09:31:09 -0600

    Add example PostConstruct and PreDestroy lifecycle methods

--- 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/springcoredemo/src/main/java/com/rossacheson/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.rossacheson.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,17 @@ public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + this.getClass().getSimpleName());
     }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff: " + this.getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff: " + this.getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes!!";
