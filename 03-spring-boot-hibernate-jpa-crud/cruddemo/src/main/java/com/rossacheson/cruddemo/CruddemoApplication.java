ba32b0eb22e48dd1f316abffd0ab759c98054b4e
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-27 13:18:09 -0600

    Create multiple students

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/CruddemoApplication.java
@@ -17,8 +17,21 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 students...");
+		Student student1 = new Student("John", "Doe", "john@test.com");
+		Student student2 = new Student("Mary", "Public", "mary@test.com");
+		Student student3 = new Student("Bonita", "Applebaum", "bonita@test.com");
+
+		System.out.println("Saving the students...");
+		studentDAO.save(student1);
+		studentDAO.save(student2);
+		studentDAO.save(student3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
