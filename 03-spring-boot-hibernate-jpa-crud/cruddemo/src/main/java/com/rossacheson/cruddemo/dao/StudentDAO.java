84c0ebcc05ac062d4418b945b26bbf1858836693
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-27 13:48:53 -0600

    Add find all method

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/dao/StudentDAO.java
@@ -2,8 +2,11 @@ package com.rossacheson.cruddemo.dao;
 
 import com.rossacheson.cruddemo.entity.Student;
 
+import java.util.List;
+
 // Student Data Access Object
 public interface StudentDAO {
     void save(Student student);
     Student findById(Integer id);
+    List<Student> findAll();
 }
--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/dao/StudentDAORepository.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/rossacheson/cruddemo/dao/StudentDAORepository.java
@@ -2,9 +2,12 @@ package com.rossacheson.cruddemo.dao;
 
 import com.rossacheson.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAORepository implements StudentDAO {
@@ -26,5 +29,13 @@ public class StudentDAORepository implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+//        TypedQuery<Student> query = entityManager.createQuery("SELECT s FROM Student s", Student.class);
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+
+        return query.getResultList();
     }
 }
