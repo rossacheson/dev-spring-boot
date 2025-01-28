499f91591813ae855f2ae043559f2e31c8120197
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-28 11:02:41 -0600

    Set students list in @PostConstruct init method

--- 04-spring-boot-rest-crud/rest-crud/src/main/java/com/rossacheson/rest_crud/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/rest-crud/src/main/java/com/rossacheson/rest_crud/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.rossacheson.rest_crud.rest;
 
 import com.rossacheson.rest_crud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<Student>();
+    private List<Student> students;
+    @PostConstruct
+    public void init() {
+        students = new ArrayList<>();
         students.add(new Student("John", "Doe"));
         students.add(new Student("Mario", "Snow"));
         students.add(new Student("May", "Trow"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }
