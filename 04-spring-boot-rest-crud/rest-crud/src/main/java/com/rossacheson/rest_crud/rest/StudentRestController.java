0722967e1bf19de5b03ca6f09ce94c81906225dc
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-28 10:56:49 -0600

    Add Students endpoint

---
+++ 04-spring-boot-rest-crud/rest-crud/src/main/java/com/rossacheson/rest_crud/rest/StudentRestController.java
@@ -0,0 +1,22 @@
+package com.rossacheson.rest_crud.rest;
+
+import com.rossacheson.rest_crud.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student("John", "Doe"));
+        students.add(new Student("Mario", "Snow"));
+        students.add(new Student("May", "Trow"));
+        return students;
+    }
+}
