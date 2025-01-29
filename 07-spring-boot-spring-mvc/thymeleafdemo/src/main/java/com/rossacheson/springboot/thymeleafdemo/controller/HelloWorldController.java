e875d78e2f7225a27c75b0942793aef7d0aab1b8
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-29 14:16:34 -0600

    Hello student

---
+++ 07-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/rossacheson/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.rossacheson.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "hellostudent";
+    }
+}
