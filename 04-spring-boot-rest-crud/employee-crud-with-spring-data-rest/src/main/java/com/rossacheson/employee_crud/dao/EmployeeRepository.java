c1ff63db58d2b8c41a906e3fb669c33c0c4972aa
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-29 09:30:25 -0600

    playing with a few more properties

--- 04-spring-boot-rest-crud/employee-crud-with-spring-data-rest/src/main/java/com/rossacheson/employee_crud/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/employee-crud-with-spring-data-rest/src/main/java/com/rossacheson/employee_crud/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.rossacheson.employee_crud.dao;
 
 import com.rossacheson.employee_crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path="empleados")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that's it ... no need to write any code LOL!
 }
