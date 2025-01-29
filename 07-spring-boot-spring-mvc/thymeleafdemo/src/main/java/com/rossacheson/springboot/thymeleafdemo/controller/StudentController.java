b622998d3e9a13efd498d6b7a7af5316ca4a83da
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-29 15:51:09 -0600

    Improve country selection

--- 07-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/rossacheson/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/rossacheson/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,18 +1,26 @@
 package com.rossacheson.springboot.thymeleafdemo.controller;
 
 import com.rossacheson.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showFrom(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
