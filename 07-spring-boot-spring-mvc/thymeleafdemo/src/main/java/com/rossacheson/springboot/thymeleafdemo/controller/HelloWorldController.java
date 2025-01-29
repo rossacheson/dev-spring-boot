0e6f125176031fce97b91aceb2d69fa6ae5f78f8
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-29 14:57:40 -0600

    Iterations...

--- 07-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/rossacheson/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/thymeleafdemo/src/main/java/com/rossacheson/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.rossacheson.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +16,25 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "hellostudent";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String processFormV2(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        name = name.toUpperCase();
+        String result = "Yo! " + name + "!";
+        model.addAttribute("message", result);
+
+        return "hellostudent";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String processFormV3(@RequestParam("studentName") String name, Model model) {
+        name = name.toUpperCase();
+        String result = "Hey friend! " + name + "!";
+        model.addAttribute("message", result);
+
         return "hellostudent";
     }
 }
