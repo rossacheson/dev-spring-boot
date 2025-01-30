9fa456571a2ae67132d7cd93de3aa149f394b914
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 07:53:16 -0600

    Min and Max validation

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
@@ -1,5 +1,7 @@
 package com.rossacheson.springboot.validationdemo;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -8,6 +10,9 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=2, message="must have at least 2 characters")
     private String lastName;
+    @Min(value=0, message="Must be greater than or equal to zero")
+    @Max(value=10, message="Must be less than or equal to ten")
+    private int freePasses;
     public Customer(){}
 
     public String getFirstName() {
@@ -24,5 +29,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
