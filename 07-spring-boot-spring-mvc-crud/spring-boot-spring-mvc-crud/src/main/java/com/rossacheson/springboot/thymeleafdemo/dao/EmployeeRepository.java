dbd69b904e8ba310d034ab3d4b68d57e94401396
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 12:28:18 -0600

    Retrieve employees sorted by Last Name

--- 07-spring-boot-spring-mvc-crud/spring-boot-spring-mvc-crud/src/main/java/com/rossacheson/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ 07-spring-boot-spring-mvc-crud/spring-boot-spring-mvc-crud/src/main/java/com/rossacheson/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -3,8 +3,13 @@ package com.rossacheson.springboot.thymeleafdemo.dao;
 import com.rossacheson.springboot.thymeleafdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
+
+    // Spring Data JPA creates the query automagically based on the method name
+    public List<Employee> findAllByOrderByLastNameAsc();
 
 }
