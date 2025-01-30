9036d16b4951755cd7559b1c4583a6254d6a8343
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 08:01:33 -0600

    Regex validation

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
@@ -1,9 +1,6 @@
 package com.rossacheson.springboot.validationdemo;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
@@ -13,6 +10,8 @@ public class Customer {
     @Min(value=0, message="Must be greater than or equal to zero")
     @Max(value=10, message="Must be less than or equal to ten")
     private int freePasses;
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
     public Customer(){}
 
     public String getFirstName() {
@@ -37,5 +36,13 @@ public class Customer {
 
     public void setFreePasses(int freePasses) {
         this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
