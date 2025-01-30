297aa6162ee9cad2d9d1e22d3f7610a6bc5f268a
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 08:05:23 -0600

    Use Integer instead of int to facilitate better null handling

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/rossacheson/springboot/validationdemo/Customer.java
@@ -7,9 +7,10 @@ public class Customer {
     @NotNull(message="is required")
     @Size(min=2, message="must have at least 2 characters")
     private String lastName;
+    @NotNull(message="is required")
     @Min(value=0, message="Must be greater than or equal to zero")
     @Max(value=10, message="Must be less than or equal to ten")
-    private int freePasses;
+    private Integer freePasses;
     @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
     public Customer(){}
@@ -30,11 +31,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
