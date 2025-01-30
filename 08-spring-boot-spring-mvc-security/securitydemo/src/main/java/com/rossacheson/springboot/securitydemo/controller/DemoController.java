75f774bcb4e908899e637c04b8d862e5ef42349f
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 13:34:03 -0600

    Basic home page

---
+++ 08-spring-boot-spring-mvc-security/securitydemo/src/main/java/com/rossacheson/springboot/securitydemo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.rossacheson.springboot.securitydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
