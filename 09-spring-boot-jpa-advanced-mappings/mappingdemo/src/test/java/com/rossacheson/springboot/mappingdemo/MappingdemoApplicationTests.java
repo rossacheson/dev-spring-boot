1406c0497d74f060c56db4e17f1b6e6e1d9303b6
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-31 10:06:51 -0600

    Starter files for section 9

---
+++ 09-spring-boot-jpa-advanced-mappings/mappingdemo/src/test/java/com/rossacheson/springboot/mappingdemo/MappingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossacheson.springboot.mappingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
