cffdcb74c17c0176c858f9cc1b4cec0912153ce1
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-31 10:12:15 -0600

    Datasource config and Hello World

--- 09-spring-boot-jpa-advanced-mappings/mappingdemo/src/main/java/com/rossacheson/springboot/mappingdemo/MappingdemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/mappingdemo/src/main/java/com/rossacheson/springboot/mappingdemo/MappingdemoApplication.java
@@ -1,7 +1,10 @@
 package com.rossacheson.springboot.mappingdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MappingdemoApplication {
@@ -10,4 +13,10 @@ public class MappingdemoApplication {
 		SpringApplication.run(MappingdemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
