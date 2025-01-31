8219bf4638f042af22cf93b6c61464611089e907
Ross Acheson <rossacheson@users.noreply.github.com>
2025-01-30 18:13:55 -0600

    Show error message, and add a fancier login form

--- 08-spring-boot-spring-mvc-security/securitydemo/src/main/java/com/rossacheson/springboot/securitydemo/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/securitydemo/src/main/java/com/rossacheson/springboot/securitydemo/controller/LoginController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("my-login")
     public String myLogin() {
-        return "plain-login";
+        return "fancy-login";
     }
 }
